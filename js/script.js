94da04f7ae67d4ee2fc914523e3a022bfd10e3d3
tentional <121067341+tentional@users.noreply.github.com>
2023-07-28 18:37:47 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// let a = parseInt(prompt('Перше значення', '0'))
+// let b = parseInt(prompt('Друге значення', '0'))
+// let c = a+b
+// document.write(`Сума чисел a i b = ${c}`)
+
+//------------------------------------------------------------------
+// let a=9;
+// let b=10
+// let   sum=a+b
+// alert("sum = "+sum)
+// let name = 'john', time = 'зараз'
+// document.write(`Привіт ${name}, де ти ${time}`)   
+
+//------------------------------------------------------------------
+//Підрахувати загальну вагу ящиків картоплі
+// let boxCount = parseInt(prompt('Кількість ящиків з картоплею'))
+// let boxWeight = parseFloat(prompt('Вага одного ящика'))
+
+// let sum = boxCount * boxWeight
+
+// document.write(`Total weight = ${sum}`)
+
+//------------------------------------------------------------------
+
